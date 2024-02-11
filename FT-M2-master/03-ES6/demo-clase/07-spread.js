a71f95e0401bdfd7d0a4d0892f4866a1d0d33a3e
Domenico Pagano <dpaganoh@gmail.com>
2024-02-11 09:35:19 -0500

    es6

--- FT-M2-master/03-ES6/demo-clase/07-spread.js
+++ FT-M2-master/03-ES6/demo-clase/07-spread.js
@@ -1,1 +1,53 @@
 // Spread Operator
+
+// con arreglos... -------------------------------------------------------------------------------------------
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5, 6];
+
+console.log(arr1)
+
+const referenciaArr1 = arr1
+console.log(referenciaArr1)
+// 'referenciaArr1' toma como valor la referencia a 'arr1'
+
+const nuevoArr = [...arr1]
+console.log(nuevoArr)
+// 'nuevoArr' toma los elementos de arr1 y los hace propios (no son instancias del original)
+
+// Los cambios efectuados sobre 'arr1' también afectarán a 'referenciaArr1'
+arr1.push(200)
+arr1.push(500)
+arr1.push(900)
+
+console.log(arr1)
+console.log(referenciaArr1)
+console.log(nuevoArr)
+
+//---
+
+const numbers = [...nuevoArr,...arr2,7,8,9]
+numbers.push(10)
+numbers.unshift(0)
+console.log(numbers)
+
+            let half = Math.floor(numbers.length / 2)
+            let mitad1 = numbers.slice(0,half)
+            let mitad2 = numbers.slice(half)
+            console.log(mitad1)
+            console.log(mitad2)
+
+
+// con objetos... -------------------------------------------------------------------------------------------
+const persona = {
+    nombre: 'Pedro',
+    edad: 20,
+    active: false
+};
+
+const personaConApellido = {
+    ...persona,                     // Toma las propiedades de 'persona' y las hace suyas.
+    apellido: 'Páramo',             // Asigna nuevas propiedades no incluidas en 'persona'.
+    active: true                    // Modifica el valor de una propiedad traida de 'persona'.
+};
+
+console.log(personaConApellido)
--- FT-M2-master/03-ES6/homework/src/selector.js
+++ FT-M2-master/03-ES6/homework/src/selector.js
@@ -1,14 +1,21 @@
-var traverseDomAndCollectElements = function (matchFunc, startEl) {
+var traverseDomAndCollectElements = function (matchFunc, startEl = document.body) {
   var resultSet = [];
 
-  if (typeof startEl === "undefined") {
+/*   if (typeof startEl === "undefined") {
     startEl = document.body;
-  }
+  } */
 
   // recorre el árbol del DOM y recolecta elementos que matchien en resultSet
   // usa matchFunc para identificar elementos que matchien
 
   // TU CÓDIGO AQUÍ
+
+  if(matchFunc(startEl)) resultSet.push(startEl)
+  for(let i=0; i < startEl.children.length; i++){
+    let result = traverseDomAndCollectElements(matchFunc,startEl.children[i])
+    resultSet = [...resultSet, ...result]
+  }
+  return resultSet
 };
 
 // Detecta y devuelve el tipo de selector
@@ -16,6 +23,20 @@ var traverseDomAndCollectElements = function (matchFunc, startEl) {
 
 var selectorTypeMatcher = function (selector) {
   // tu código aquí
+  if(selector[0] === '.') return 'class'
+  else if(selector[0] === '#') return 'id'
+
+    // opción 1:
+  /* for(let i=0; i < selector.length; i++){
+    if(selector[i] === '.') return 'tag-class'
+  } */
+    // opción 2:
+  //else if(selector.split('.').length === 2) return 'tag.class'
+    // opción 3:
+  //else if(selector.includes('.')) return 'tag.class'
+
+  else if(selector.includes('.')) return 'tag.class'
+  return 'tag'
 };
 
 // NOTA SOBRE LA FUNCIÓN MATCH
@@ -24,19 +45,37 @@ var selectorTypeMatcher = function (selector) {
 // matchea el selector.
 
 var matchFunctionMaker = function (selector) {
-  var selectorType = selectorTypeMatcher(selector);
+  var selectorType = selectorTypeMatcher(selector);   // 'id', 'class', 'tag', 'tag.class'
   var matchFunction;
   if (selectorType === "id") {
+    matchFunction = function(elem){
+      return '#' + elem.id === selector
+    }
   } else if (selectorType === "class") {
+    matchFunction = function(elem){
+      for(let i=0; i < elem.classList.length; i++){
+        if(elem.classList[i] === selector) return true
+      }
+      return false
+    }
   } else if (selectorType === "tag.class") {
+    matchFunction = function(elem){
+      let [t,c] = selector.split('.')   // t=tag, c=class
+      return matchFunction(t)(elem) && matchFunction(c)(elem)   // --> booleano
+    }
   } else if (selectorType === "tag") {
+    matchFunction = function(elem){
+      return elem.tagName.toLowerCase() === selector.toLowerCase    // --> booleano
+    }
   }
-  return matchFunction;
+  return matchFunction;   // function
 };
 
 var $ = function (selector) {
   var elements;
-  var selectorMatchFunc = matchFunctionMaker(selector);
+  var selectorMatchFunc = matchFunctionMaker(selector); // function
   elements = traverseDomAndCollectElements(selectorMatchFunc);
   return elements;
 };
+
+// $('.photos') /* Es igual a */ document.querySelectorAll('.photos')
