1417b05317cc8abb8111186f010cdfcf7636cb70
Domenico Pagano <dpaganoh@gmail.com>
2024-02-20 13:15:06 -0500

    AJAX

--- FT-M2-master/04-Ajax/homework/index.js
+++ FT-M2-master/04-Ajax/homework/index.js
@@ -1,3 +1,5 @@
+// LISTA DE AMIGOS ----------------------------------- ----------------------------------- -----------------------------------
+
 const URL = 'http://localhost:5000/amigos'          //captura la URL
 const img = $('img')                                //captura el elemento de tipo imagen    --  <img>
 const lista = $('#lista')                           //captura el elemento con id='lista'    --  <ul>
@@ -23,14 +25,14 @@ const obtenerAmigos = () => {
 // FUNCIÓN click
 boton.click(obtenerAmigos)
 
-//----------------------------------- ----------------------------------- -----------------------------------
+// BUSCAR AMIGO POR ID ----------------------------------- ----------------------------------- -----------------------------------
 
 const search = $('#search')     //captura el elemento con id='search'   --  <button>
 const input = $('#input')       //captura el elemento con id='input'    --  <input>
 const friend = $('#amigo')      //captura el elemento con id='amigo'    --  <span>
 
 // FUNCIÓN que limpia el input.
-const cleanInputs = () => {
+const cleanInputs = (input) => {
     input.val('')               //limpia el input
     input.focus()               //enfoca el cursor en el input
 }
@@ -52,12 +54,31 @@ const showFriend = () => {
     if(!id) return friend.text('No hay un id que buscar')               //si no hay id que buscar (input vacio) entrega mensaje.
     //console.log(id)
     $.get(`${URL}/${id}`, nameFriend).fail(friendNotFound())            //trae amigo o mensaje de error.
-    cleanInputs()                                                       //limpia inpit && foco en el input.
+    cleanInputs(input)                                                       //limpia inpit && foco en el input.
+    
 }
 
 // FUNCIÓN click
 search.click(showFriend)
 
-//----------------------------------- ----------------------------------- -----------------------------------
+// BORRAR AMIGO ----------------------------------- ----------------------------------- -----------------------------------
 
-//continúa en DELETE - 1:08:00
+const inputDelete = $('#inputDelete')           //captura el elemento con id='inputDelete'   --  <input>
+const btnDelete = $('#delete')                  //captura el elemento con id='delete'        --  <button>
+const success = $('#success')                   //captura el elemento con id='success'       --  <span>
+
+const deleteFriend = () => {
+    const idDelete = inputDelete.val()
+    if(!idDelete) return success.text('Ingresa el id del amigo')
+    $.ajax({
+        url: `${URL}/${idDelete}`,
+        type: 'DELETE',
+        success: () => {
+            success.text(`el amigo con id ${idDelete} ha sido borrado con exito.`)
+            obtenerAmigos()
+            cleanInputs(inputDelete)
+        }
+    })
+}
+
+btnDelete.click(deleteFriend)
