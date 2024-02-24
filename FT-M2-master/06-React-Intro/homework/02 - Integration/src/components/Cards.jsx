8080546bea6e48157e67ad3fe2ffd49c856866a8
Domenico Pagano <dpaganoh@gmail.com>
2024-02-24 09:00:36 -0500

    ReactHW1

--- FT-M2-master/06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ FT-M2-master/06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -1,9 +1,22 @@
 import Card from './Card';
 
-export default function Cards(props) {
+export default function Cards({characters}) {
    return (
    <div>
-      
+      {
+         characters.map(char => (
+            <Card 
+               key={char.id}
+               name={char.name}
+               status={char.status}
+               species={char.species}
+               gender={char.gender}
+               origin={char.origin.name}
+               image={char.image}
+               onClose={() => window.alert('Emulamos que se cierra la card de cards')}
+            />
+         ))
+      }
    </div>
    );
 }
