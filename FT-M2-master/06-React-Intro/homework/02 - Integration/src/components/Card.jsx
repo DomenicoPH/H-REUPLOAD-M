ae1935afb329dca248c530e05fea1f3ea279e7ea
Domenico Pagano <dpaganoh@gmail.com>
2024-02-23 21:39:03 -0500

    React-hw2

--- FT-M2-master/06-React-Intro/homework/02 - Integration/src/components/Card.jsx
+++ FT-M2-master/06-React-Intro/homework/02 - Integration/src/components/Card.jsx
@@ -1,13 +1,30 @@
 export default function Card(props) {
    return (
       <div>
-         {/* <button onClick={}>X</button>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <img src={} alt='' /> */}
+         <button onClick={props.onClose}>X</button>
+         <h2>{props.name}</h2>
+         <h2>{props.status}</h2>
+         <h2>{props.species}</h2>
+         <h2>{props.gender}</h2>
+         <h2>{props.origin}</h2>
+         <img src={props.image} alt='' />
       </div>
    );
 }
+
+
+// desctructuring:
+/* export default function Card({onClose, name, status, species, gender, origin, image}) {
+   return (
+      <div>
+         <button onClick={onClose}>X</button>
+         <h2>{name}</h2>
+         <h2>{status}</h2>
+         <h2>{species}</h2>
+         <h2>{gender}</h2>
+         <h2>{origin}</h2>
+         <img src={image} alt='' />
+      </div>
+   );
+}
+ */
