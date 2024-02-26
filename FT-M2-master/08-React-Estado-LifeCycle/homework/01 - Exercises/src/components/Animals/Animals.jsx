ccfd26cb1ede786362460340818f9ff80edc2893
Domenico Pagano <dpaganoh@gmail.com>
2024-02-26 17:38:12 -0500

    LifeCycle

--- FT-M2-master/08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
+++ FT-M2-master/08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
-// import styledAnimals from './Animals.module.css'
+import styledAnimals from './Animals.module.css'
 
 export default class Animals extends React.Component {
   constructor(props) {
-    super();
+    super(props);
   }
 
   render() {
-    return <div></div>
+    console.log(this.props)
+    const animals = this.props.animals
+    return <div className={styledAnimals.container}>
+      {animals.map((animal,index) => {
+        return (
+          <div className={styledAnimals.containerAnimals} key={index}>
+            <h5>{animal.name}</h5>
+            <img src={animal.image} alt={animal.name} width='300px'/>
+            <span>{animal.specie}</span>
+          </div>
+        )
+      })}
+    </div>
   }
 }
--- FT-M2-master/08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ FT-M2-master/08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -3,15 +3,69 @@ import React from "react";
 import Animals from "../Animals/Animals";
 // eslint-disable-next-line no-unused-vars
 import Species from "../Species/Species";
-// import styledZoo from "./Zoo.module.css";
+import styledZoo from "./Zoo.module.css";
 
 export default function Zoo() {
   /* Escribe acá tu código */
 
+  const [zoo, setZoo] = React.useState({
+    zooName: '',
+    animals: [],
+    species: [],
+    allAnimals: []
+  })
+
+  const handleInputChange = (event) => {
+    setZoo({
+      ...zoo,
+      zooName: event.target.value
+    })
+  }
+
+  const handleSpecies = (event) => {
+    console.log(event.target.value)
+    setZoo({
+      ...zoo,
+      animals: zoo.allAnimals.filter(
+        animal => animal.specie === event.target.value
+      )
+    })
+  }
+
+  const handleAllSpecies = (event) => {
+    setZoo({
+      ...zoo,
+      animals: zoo.allAnimals
+    })
+  }
+
+  React.useEffect(() => { // Hace solicitud a la API y llena el estado con la información que se trae.
+    fetch('http://localhost:3001/zoo')
+    .then((res) => res.json())
+    .then((data) =>
+       setZoo({
+          ...zoo,
+          animals: data.animals,
+          species: data.species,
+          allAnimals: data.animals,
+       })
+    )
+    .catch((error) => console.log(error));
+  },[])
 
   return (
-    <div>
-      <h1>-</h1>
+    <div className={styledZoo.divContentTitle}>
+        <label htmlFor="zooName">Zoo Name:</label>
+        <input type="text" id="zooName" value={zoo.zooName} onChange={handleInputChange}/>
+        <h1 className={styledZoo.title}>{zoo.zooName}</h1>
+      <Species 
+        species={zoo.species} 
+        handleSpecies={handleSpecies} 
+        handleAllSpecies={handleAllSpecies}
+      />
+      <Animals 
+        animals={zoo.animals} 
+      />
     </div>
   );
 }
