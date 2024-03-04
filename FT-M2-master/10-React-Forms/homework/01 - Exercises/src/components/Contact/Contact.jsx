66161c8b00ddaa806c48413d6d5421f866c48bff
Domenico Pagano <dpaganoh@gmail.com>
2024-03-04 09:32:21 -0500

    react-forms-hw

--- FT-M2-master/10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
+++ FT-M2-master/10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
@@ -1,9 +1,55 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Contact.modules.css'
 
 // eslint-disable-next-line
 const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
 export default function Contact () {
-  return <div>Crear Formulario</div>
+
+  const [inputs, setInputs] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const handleChange = (e) => {
+    const property = e.target.name;
+    const value = e.target.value
+    setInputs({...inputs, [property]: value})
+  }
+
+  return <div>
+    <form>
+      
+      <label>Nombre:</label>
+      <input 
+        name='name'
+        placeholder='Escribe tu nombre...'
+        type="text"
+        value={inputs.name}
+        onChange={handleChange}
+      />
+      
+      <label>Correo Electrónico:</label>
+      <input
+        name='email'
+        placeholder='Escribe tu email...' 
+        type="text"
+        value={inputs.email}
+        onChange={handleChange}
+      />
+
+      <label>Mensaje:</label>
+      <textarea 
+        name='message'
+        placeholder='Escribe tu mensaje...'
+        type="text"
+        value={inputs.message}
+        onChange={handleChange}
+      />
+
+      <button type='submit'>Enviar</button>
+
+    </form>
+  </div>
 }
