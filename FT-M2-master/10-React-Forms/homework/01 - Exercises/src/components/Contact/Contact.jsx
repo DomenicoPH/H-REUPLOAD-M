f0871f2e8e1e1a4359a492adac49e5cbf0b1908b
Domenico Pagano <dpaganoh@gmail.com>
2024-03-04 20:27:51 -0500

    react-forms

--- FT-M2-master/10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
+++ FT-M2-master/10-React-Forms/homework/01 - Exercises/src/components/Contact/Contact.jsx
@@ -4,9 +4,23 @@ import './Contact.modules.css'
 // eslint-disable-next-line
 const regexEmail = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
 
+export const validate = (inputs) => {
+  const errors = {}
+  if(!inputs.name){errors.name = 'Se requiere un nombre'}
+  if(!regexEmail.test(inputs.email)){errors.email = 'Debe ser un correo electrónico'}
+  if(!inputs.message){errors.message = 'Se requiere un mensaje'}
+  return errors;
+}
+
 export default function Contact () {
 
   const [inputs, setInputs] = useState({
+    name: '',
+    email: '',
+    message: '',
+  });
+
+  const [errors, setErrors] = useState({
     name: '',
     email: '',
     message: '',
@@ -16,10 +30,27 @@ export default function Contact () {
     const property = e.target.name;
     const value = e.target.value
     setInputs({...inputs, [property]: value})
+    setErrors(validate({...inputs, [property]: value}))
   }
 
-  return <div>
-    <form>
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if(Object.keys(errors).length){
+      alert('Debe llenar todos los campos')
+    } else {
+      alert('Datos completos')
+      setInputs({
+        name: '',
+        email: '',
+        message: '',
+      })
+    }
+  }
+
+  return (
+  <div>
+
+    <form onSubmit={handleSubmit}>
       
       <label>Nombre:</label>
       <input 
@@ -28,7 +59,9 @@ export default function Contact () {
         type="text"
         value={inputs.name}
         onChange={handleChange}
+        className={errors.name ? 'warning' : null}
       />
+      <p className='danger'>{errors.name && errors.name}</p>
       
       <label>Correo Electrónico:</label>
       <input
@@ -37,7 +70,9 @@ export default function Contact () {
         type="text"
         value={inputs.email}
         onChange={handleChange}
+        className={errors.email ? 'warning' : null}
       />
+      <p className='danger'>{errors.email && errors.email}</p>
 
       <label>Mensaje:</label>
       <textarea 
@@ -46,10 +81,14 @@ export default function Contact () {
         type="text"
         value={inputs.message}
         onChange={handleChange}
+        className={errors.message && 'warning'}
       />
+      <p className='danger'>{errors.message && errors.message}</p>
 
       <button type='submit'>Enviar</button>
 
     </form>
+
   </div>
+  )
 }
