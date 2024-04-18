8fc4621ae6ef61ff62cd30c405d595566b8b129c
Domenico Pagano <dpaganoh@gmail.com>
2024-04-17 19:26:38 -0500

    node-hw

--- FT-M3-master/01-Node/homework/01 - Exercises/bash.js
+++ FT-M3-master/01-Node/homework/01 - Exercises/bash.js
@@ -2,7 +2,17 @@ const process = require('process');
 const { Z_ASCII } = require('zlib');
 const commands = require('./commands/index.js');
 
-function bash() {}
+function print(output) {
+   process.stdout.write(output);
+   process.stdout.write("\nprompt >");
+};
+
+function bash() {
+   process.stdout.write("prompt >");
+   process.stdout.write("data",(data) => {
+      let args = 
+   })
+};
 
 bash();
 module.exports = {
